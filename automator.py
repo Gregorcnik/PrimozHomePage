08f851c19ec240299b2809ebf8744872c025095b
Gregorcnik <gregor.pot.gm@gmail.com>
2022-03-02 18:30:56 +0100

    Update and create files

--- automator.py
+++ automator.py
@@ -5,6 +5,32 @@ def oklepaj(string, tag, zamik, Class='', Id=''):
         Id = ' '+Id
     return f'{(zamik-1)*"  "}<{tag}{Class}{Id}>\n{zamik*"  "}{string}\n{(zamik-1)*"  "}</{tag}>'
 
-summary = input()
-text = input()
-print(oklepaj(oklepaj(summary, 'summary', 2)+'\n'+oklepaj(text, 'p', 2), 'details', 0))
+f = open('indexTest.html', 'r')
+index=[];
+for vrstica in f:
+    index.append(vrstica)
+f.close()
+
+f = open('vsebina.txt', 'r')
+vsebina=[];
+for vrstica in f:
+    vsebina.append(vrstica.replace('\n', ''))
+f.close()
+
+dodatek=''
+for i in range(0, len(vsebina), 2):
+    dodatek+=oklepaj(oklepaj(vsebina[i], 'summary', 0)+'\n'+oklepaj(vsebina[i+1], 'p', 0), 'details', 0)+'\n'
+dodatek+='\n'
+
+print(dodatek)
+
+dodajaj = index.index('<!-- dodajaj -->\n')
+index.insert(dodajaj, dodatek)
+
+indexStr=''
+for i in range(0, len(index)):
+    indexStr+=index[i]
+
+f=open('indexTest.html', 'w')
+f.write(indexStr)
+f.close();
